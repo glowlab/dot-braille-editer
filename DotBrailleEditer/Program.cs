98eb78b7d08bd36ed7156501138fed55938ce875
ABitGlow <bitglow@outlook.com>
2020-12-31 20:21:33 +0800

    Create a new project.

---
+++ DotBrailleEditer/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DotBrailleEditer
+{
+    class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+        }
+    }
+}
