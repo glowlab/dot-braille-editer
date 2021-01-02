9636ed69896fdec37dc891dca822833ed88fc527
ABitGlow <bitglow@outlook.com>
2021-01-02 17:43:12 +0800

    Add DocumentForm.

---
+++ DotBrailleEditer/Views/DocumentForm.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace DotBrailleEditer.Views
+{
+    public partial class DocumentForm : Form
+    {
+        public DocumentForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
